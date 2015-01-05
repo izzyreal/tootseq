53d2c303a8508525e9f87de3afff833f38b93fd3
TootSoftware@googlemail.com <TootSoftware@googlemail.com@e9dce775-203d-6ee6-6ea2-073725ac2d10>
2015-01-05 12:10:23 +0000

    Update copyright for 2015

--- tootseq/src/uk/org/toot/seq/Source.java
+++ tootseq/src/uk/org/toot/seq/Source.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2009, 2010, 2014 Steve Taylor.
+// Copyright (C) 2009, 2010, 2014 2015 Steve Taylor.
 // Distributed under the Toot Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.toot.org.uk/LICENSE_1_0.txt)
